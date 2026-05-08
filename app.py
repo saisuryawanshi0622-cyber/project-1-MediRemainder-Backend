10b1bc3cc02fb1a9b966de8db88827c31ea9ae1c
medi <saisuryawanshi0622@gamil.com>
2026-05-09 01:02:18 +0530

    local hosting link

--- app.py
+++ app.py
@@ -267,4 +267,5 @@ def get_logs(user_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5001, use_reloader=False)
+    port = int (os.environ.get("PORT", 5001))
+    app.run(debug=True, port=port, use_reloader=False, use_reloader=False) 
